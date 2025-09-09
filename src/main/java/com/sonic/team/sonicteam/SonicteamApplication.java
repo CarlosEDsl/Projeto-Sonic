0d27f62d6a36a2f25594deea8bd62fbe9ffbd39f
CarlosEDsl <carloseduardoestds@gmail.com>
2025-09-09 19:24:17 -0300

    First Commit

---
+++ src/main/java/com/sonic/team/sonicteam/SonicteamApplication.java
@@ -0,0 +1,13 @@
+package com.sonic.team.sonicteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonicteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonicteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonic/team/sonicteam/SonicteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonic.team.sonicteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonicteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
