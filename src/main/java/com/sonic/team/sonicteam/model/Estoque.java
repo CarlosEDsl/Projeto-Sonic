b48e452cabafffe5d8381bae492b53d693573de3
CarlosEDsl <carloseduardoestds@gmail.com>
2025-11-09 19:10:05 -0300

    Iniciando classes principais e desenvolvendo rota do Empréstimo

--- src/main/java/com/sonic/team/sonicteam/model/Estoque.java
+++ src/main/java/com/sonic/team/sonicteam/model/Estoque.java
@@ -2,7 +2,6 @@ package com.sonic.team.sonicteam.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
