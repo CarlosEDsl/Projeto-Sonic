0a5c3060174cd26bef194943e0de43d8777a521c
Michelyserras <michelysrrs@gmail.com>
2025-11-15 13:29:26 -0300

    feat: add métodos para verificar a unicidade de editora, autor e edição no repository

--- src/main/java/com/sonic/team/sonicteam/repository/LivroRepository.java
+++ src/main/java/com/sonic/team/sonicteam/repository/LivroRepository.java
@@ -1,9 +1,12 @@
 package com.sonic.team.sonicteam.repository;
 
 import com.sonic.team.sonicteam.model.Livro;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro,String> {
+    boolean existsByAutorAndEditoraAndEdicao(String autor, String editora, String edicao);
+    Optional<Livro> findByAutorAndEditoraAndEdicao(String autor, String editora, String edicao);
 }
