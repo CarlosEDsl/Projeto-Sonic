26852be4b11f4ca6b5c0f854505f7a031dca7603
Malu Abrami <marialuizaabrami@gmail.com>
2025-11-16 15:30:54 -0300

    feat: add service para estoque

---
+++ src/main/java/com/sonic/team/sonicteam/service/estoque/EstoqueService.java
@@ -0,0 +1,78 @@
+package com.sonic.team.sonicteam.service.estoque;
+
+import com.sonic.team.sonicteam.exception.ExemplarNaoEstaDisponivelException;
+import com.sonic.team.sonicteam.exception.RecursoNaoEncontradoException;
+import com.sonic.team.sonicteam.model.DTO.Estoque.AtualizarEstoqueResquestDTO;
+import com.sonic.team.sonicteam.model.DTO.Estoque.EstoqueRequestDTO;
+import com.sonic.team.sonicteam.model.DTO.Estoque.EstoqueResponseDTO;
+import com.sonic.team.sonicteam.model.Estoque;
+import com.sonic.team.sonicteam.repository.EstoqueRepository;
+import com.sonic.team.sonicteam.repository.LivroRepository;
+import com.sonic.team.sonicteam.util.EstoqueMapper;
+import com.sonic.team.sonicteam.validation.EstoqueValidator;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EstoqueService implements IEstoqueService{
+    private final EstoqueRepository estoqueRepository;
+    private final LivroRepository livroRepository;
+    private final EstoqueValidator estoqueValidator;
+
+    public EstoqueService(EstoqueRepository estoqueRepository, LivroRepository livroRepository, EstoqueValidator estoqueValidator) {
+        this.estoqueRepository = estoqueRepository;
+        this.livroRepository = livroRepository;
+        this.estoqueValidator = estoqueValidator;
+    }
+
+    @Override
+    public EstoqueResponseDTO cadastrarNovoExemplar(EstoqueRequestDTO request) {
+        var livroExiste = livroRepository.findById(request.livroIsbn());
+        if(livroExiste.isEmpty()) throw new RecursoNaoEncontradoException("Livro não encontrado com o isbn " + request.livroIsbn());
+
+        var novoEstoque = estoqueRepository.save(new Estoque(livroExiste.get()));
+        return EstoqueMapper.ToResponseDTO(novoEstoque);
+    }
+
+    @Override
+    public List<EstoqueResponseDTO> getExemplaresDisponiveis(String livroIsbn) {
+        if(livroIsbn.isEmpty() || livroIsbn.isBlank()) return EstoqueMapper.ToListResponseDTO(estoqueRepository.findAllByDisponivelIsTrue());
+
+        return EstoqueMapper.ToListResponseDTO(estoqueRepository.findAllByDisponivelIsTrueAndLivroIsbn(livroIsbn));
+    }
+
+    @Override
+    public EstoqueResponseDTO getExemplar(Long id) {
+        var exemplarEstoque = estoqueRepository.findById(id);
+
+        if(exemplarEstoque.isPresent()) return EstoqueMapper.ToResponseDTO(exemplarEstoque.get());
+
+        throw new RecursoNaoEncontradoException("Exemplar não encontrado com o id " + id);
+    }
+
+    @Override
+    public EstoqueResponseDTO atualizarDisponibilidadeExemplar(AtualizarEstoqueResquestDTO request) {
+        var exemplarEstoque = estoqueRepository.findById(request.id());
+        if(exemplarEstoque.isPresent()) {
+            exemplarEstoque.get().setDisponivel(request.disponivel());
+            estoqueRepository.save(exemplarEstoque.get());
+            return EstoqueMapper.ToResponseDTO(exemplarEstoque.get());
+        }
+
+        throw new RecursoNaoEncontradoException("Exemplar não encontrado com o id " + request.id());
+    }
+
+    @Override
+    public void deletarExemplar(Long id) {
+        try {
+            if(estoqueValidator.validarDisponibilidadeDoExemplar(id)) {
+                estoqueRepository.deleteById(id);
+            }
+            else throw new ExemplarNaoEstaDisponivelException(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RecursoNaoEncontradoException("Exemplar não encontrado com o id " + id);
+        }
+    }
+}
