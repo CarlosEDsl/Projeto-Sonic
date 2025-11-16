c1ba078515dbb15784e71c028e5d0ae75110538d
Malu Abrami <marialuizaabrami@gmail.com>
2025-11-16 15:29:28 -0300

    feat: add entidade estoque

--- src/main/java/com/sonic/team/sonicteam/model/Estoque.java
+++ src/main/java/com/sonic/team/sonicteam/model/Estoque.java
@@ -1,7 +1,6 @@
 package com.sonic.team.sonicteam.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +11,40 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Estoque {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "livro_isbn")
+    private Livro livro;
+
+    private Boolean disponivel = true;
+
+    public Estoque(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
