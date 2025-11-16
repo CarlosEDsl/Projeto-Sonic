7fa2b3fbdb9d46ce26552bd3768235b7de02c810
Malu Abrami <marialuizaabrami@gmail.com>
2025-11-16 15:31:37 -0300

    feat: add controller para estoque com seus devidos endpoints

---
+++ src/main/java/com/sonic/team/sonicteam/service/estoque/IEstoqueService.java
@@ -0,0 +1,16 @@
+package com.sonic.team.sonicteam.service.estoque;
+
+import com.sonic.team.sonicteam.model.DTO.Estoque.AtualizarEstoqueResquestDTO;
+import com.sonic.team.sonicteam.model.DTO.Estoque.EstoqueRequestDTO;
+import com.sonic.team.sonicteam.model.DTO.Estoque.EstoqueResponseDTO;
+import jakarta.validation.constraints.Null;
+
+import java.util.List;
+
+public interface IEstoqueService {
+    EstoqueResponseDTO cadastrarNovoExemplar(EstoqueRequestDTO request);
+    List<EstoqueResponseDTO> getExemplaresDisponiveis(String livroIsbn);
+    EstoqueResponseDTO getExemplar(Long id);
+    EstoqueResponseDTO atualizarDisponibilidadeExemplar(AtualizarEstoqueResquestDTO request);
+    void deletarExemplar(Long id);
+}
