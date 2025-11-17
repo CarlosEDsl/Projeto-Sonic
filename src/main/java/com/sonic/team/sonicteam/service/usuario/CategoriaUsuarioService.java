a6a078da7fff1ea23ea72e42e25d4c1d2affb779
Michelyserras <michelysrrs@gmail.com>
2025-11-17 20:43:29 -0300

    Revert no Revert

--- src/main/java/com/sonic/team/sonicteam/service/usuario/CategoriaUsuarioService.java
+++ src/main/java/com/sonic/team/sonicteam/service/usuario/CategoriaUsuarioService.java
@@ -1,11 +1,14 @@
 package com.sonic.team.sonicteam.service.usuario;
 
 import com.sonic.team.sonicteam.exception.DadoInvalidoException;
+import com.sonic.team.sonicteam.exception.RecursoNaoEncontradoException;
 import com.sonic.team.sonicteam.model.DTO.Usuario.CategoriaUsuario;
 import com.sonic.team.sonicteam.repository.CategoriaUsuarioRepository;
 import com.sonic.team.sonicteam.util.MensagensUsuario;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CategoriaUsuarioService {
@@ -20,5 +23,13 @@ public class CategoriaUsuarioService {
     public CategoriaUsuario buscarPorId(Long id) {
         return categoriaUsuarioRepository.findById(id)
                 .orElseThrow(() -> new DadoInvalidoException(MensagensUsuario.CATEGORIA_INEXISTENTE));
+    }
+
+    @Transactional(readOnly = true)
+    public List<CategoriaUsuario> listar(){
+        if(categoriaUsuarioRepository.findAll().isEmpty()){
+            throw new RecursoNaoEncontradoException("Não há nenhuma categoria cadastrada.");
+        }
+        return categoriaUsuarioRepository.findAll();
     }
 }
